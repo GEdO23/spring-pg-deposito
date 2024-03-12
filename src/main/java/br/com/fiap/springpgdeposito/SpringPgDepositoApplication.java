078cc3a89db72c5b525022dfd0fe505a8f066f2e
GEdO23 <rm99632@fiap.com.br>
2024-03-12 08:58:28 -0300

    Colocando uma pagina inicial basica agradavel ao cliente

--- src/main/java/br/com/fiap/springpgdeposito/SpringPgDepositoApplication.java
+++ src/main/java/br/com/fiap/springpgdeposito/SpringPgDepositoApplication.java
@@ -2,12 +2,29 @@ package br.com.fiap.springpgdeposito;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringPgDepositoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringPgDepositoApplication.class, args);
+    }
+
+    @GetMapping(value = "/")
+    public String index() {
+        return """
+                <main style="width: 100%; padding-top: 90px;">
+                    <h1 style="font-family: sans-serif;text-align: center;">Seja muito bem vindo a Holding Benezinho</h1>
+                    <nav>
+                        <ul>
+                            <li><a href="/produtos">Produtos</a></li>
+                        </ul>
+                    </nav>
+                </main>
+                """;
     }
 
 }
